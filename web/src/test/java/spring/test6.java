cb51177aa302946e7e4f5a1aeec5f174ca302823
wan <2690794644@qq.com>
2020-11-18 09:39:33 +0800

    one

---
+++ web/src/test/java/spring/test6.java
@@ -0,0 +1,31 @@
+package spring;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import spring.entity.SysActivitiFormKey;
+import spring.util.IdWorker;
+
+/**
+ * @author shen
+ * @date 2020/10/15 13:35
+ */
+public class test6 {
+    //填写formkey的工具
+    public static void main(String[] args) {
+        SysActivitiFormKey sysActivitiFormKey=new SysActivitiFormKey();
+        sysActivitiFormKey.setActivitiType("2");
+        sysActivitiFormKey.setIsAutoDispose("2");
+        sysActivitiFormKey.setAssigneeClass("spring.listener.impl.WsVisitCountryAssignee");
+        System.out.println(JSON.toJSONString(sysActivitiFormKey, SerializerFeature.WriteMapNullValue));
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+        System.out.println(IdWorker.getIdWorkerNext().toString());
+
+    }
+
+}
